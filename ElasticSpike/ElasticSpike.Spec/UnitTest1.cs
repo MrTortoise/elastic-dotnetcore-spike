83e21592468583b434efdf66ef52527b941a178c
John Nicholas <themrtortoise@gmail.com>
2018-09-24 21:45:21 +0100

    initial

---
+++ ElasticSpike/ElasticSpike.Spec/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ElasticSpike.Spec
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
