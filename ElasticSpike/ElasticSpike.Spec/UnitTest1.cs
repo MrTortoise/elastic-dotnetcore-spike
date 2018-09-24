664256a333c833b33acfd9acd1a694c229f44aa3
John Nicholas <themrtortoise@gmail.com>
2018-09-24 22:16:31 +0100

    damnit, timing problem

--- ElasticSpike/ElasticSpike.Spec/UnitTest1.cs
+++ ElasticSpike/ElasticSpike.Spec/UnitTest1.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Linq;
+using Elasticsearch.Net;
+using Nest;
 using Xunit;
 
 namespace ElasticSpike.Spec
@@ -8,7 +11,53 @@ namespace ElasticSpike.Spec
         [Fact]
         public void Test1()
         {
+            var uris = new[]
+            {
+                new Uri("http://localhost:9200")
+            };
+
+            var connectionPool = new SniffingConnectionPool(uris);
+            var settings = new ConnectionSettings(connectionPool)
+                .DefaultIndex("products");
             
+            var client = new ElasticClient(settings);
+
+            var product = new Product(1, 3);
+
+            var response = client.IndexDocument(product);
+            
+            var searchResponse = client.Search<Product>(s => s
+                .From(0)
+                .Size(10)
+                .Query(q => q
+                    .Match(m => m
+                        .Field(f => f.Id)
+                        .Query("1")
+                    )
+                )
+            );
+
+            var people = searchResponse.Documents;
+            
+            Assert.True(people.Any());
+
+
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(int id, double price)
+        {
+            Id = id;
+            Price = price;
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# so elastic was wierd at work with eventual consistency problem on create 
+
+
+http://localhost:5601/app/kibana#
+http://localhost:9200/
